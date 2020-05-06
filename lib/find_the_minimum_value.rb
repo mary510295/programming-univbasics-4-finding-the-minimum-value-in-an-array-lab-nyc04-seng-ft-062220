c84f77f53ff5c915c8975d505a07ecd8848fd1dc
Fu <fu.goto@gmail.com>
2020-05-06 01:14:13 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,11 +1,11 @@
 def find_min_value(array)
-  min = 0
-  i = 0
+  min = array[0]
+  i = 1
   while i < array.length do
     if array[i] < min 
       min = array[i]
     end
     i += 1
-end
+  end
 min
 end
